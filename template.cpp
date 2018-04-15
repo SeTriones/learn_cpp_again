a2e4a431adbd5aa453375ef533bb35bb3cef32c0
Bin CHEN <setriones@gmail.com>
2018-04-15 14:27:39 +0800

    finish template packet

--- template.cpp
+++ template.cpp
@@ -3,19 +3,30 @@
 
 using namespace std;
 
+template <typename T>
+void foo(const T &t) {
+	cout << "end " << t << endl;
+}
+
 template <typename T, typename... Args>
 void foo(const T &t, Args & ... args) {
 	cout << sizeof...(Args) << endl;
 	cout << sizeof...(args) << endl;
+	cout << t << endl;
+	foo(args...);
 	return;
 }
+
 
 void test_template_parameter_packet() {
 	int i = 0;
 	float b = 3.2f;
 	string s("test");
+	cout << "1 paramter" << endl;
 	foo(i);
+	cout << "1+1 paramters" << endl;
 	foo(i, b);
+	cout << "1+2 paramters" << endl;
 	foo(i, b, s);
 }
 
